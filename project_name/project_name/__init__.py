ba600bde560d7d295e659bcce02f32d81a4ef951
Marc-Olivier Titeux <marcoliviertiteux@gmail.com>
2013-11-06 21:45:13 +0100

    Update __init__.py

--- project_name/project_name/__init__.py
+++ project_name/project_name/__init__.py
@@ -1,12 +1,1 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# Created on 7/28/13
 
-"""
-
-"""
-
-__authors__ = [# alphabetical order by last name, please
-               '"Marc-Olivier Titeux" <marcolivier.titeux@gmail.com>',
-]
